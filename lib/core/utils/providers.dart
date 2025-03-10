f61f7680022d121180a11a33e3ba99c1a7d1ed78
alperburaak <alperburaak@outlook.com>
2025-03-10 06:00:31 +0300

    core

---
+++ lib/core/utils/providers.dart
@@ -0,0 +1,7 @@
+import 'package:offline_courier_app/providers/delivery_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+
+List<SingleChildWidget> providers = [
+  ChangeNotifierProvider(create: (_) => DeliveryProvider()),
+];
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:offline_courier_app/core/utils/providers.dart';
+import 'package:offline_courier_app/core/utils/theme.dart';
+import 'package:offline_courier_app/ui/pages/home_page.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: providers,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Offline Kargo Teslimatı',
+        theme: appTheme,
+        home: HomePage(),
+      ),
+    );
+  }
+}
